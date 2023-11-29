a4ebe1a4e81190ecf1157dc3b3a4a7ba7b6c6384
lunarinaa <aydan.akbarova@gmail.com>
2023-11-29 01:43:34 -0800

    New version of printf using functions

--- _printf.c
+++ _printf.c
@@ -1,20 +1,55 @@
 #include "main.h"
+/**
+ * print_char - Helper function to print a character
+ * @c: Character to be printed
+ *
+ * Return: Number of characters printed
+ */
+int print_char(int c)
+{
+	_putchar(c);
+	return (1);
+}
 
 /**
+ * print_string - Helper function to print a string
+ * @str: String to be printed
  *
- *
- *
+ * Return: Number of characters printed
  */
+int print_string(char *str)
+{
+	int count = 0;
 
+	if (str == NULL)
+		str = "(null)";
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+ * _printf - Custom printf function
+ * @format: String with format specifiers
+ *
+ * Return: Number of characters printed
+ */
 int _printf(const char *format, ...)
 {
 	int count = 0, c;
-
 	va_list args;
 
 	if (format == NULL)
 		return (-1);
+
 	va_start(args, format);
+
 	while (*format != '\0')
 	{
 		if (*format == '%')
@@ -23,38 +58,25 @@ int _printf(const char *format, ...)
 			if (*format == '\0')
 				break;
 			if (*format == '%')
-			{
-				_putchar('%');
-				count++;
-			}	
+				count += print_char('%');
 			else if (*format == 'c')
-			{	
+			{
 				c = va_arg(args, int);
-				_putchar(c);
-				count++;
+				count += print_char(c);
 			}
 			else if (*format == 's')
-			{	
+			{
 				char *str = va_arg(args, char*);
-	
-				if (str == NULL)
-					str = "(null)";
-				while (*str != '\0')
-				{
-					_putchar(*str);
-					str++;
-					count++;
-				}	
-			}
 
+				count += print_string(str);
+			}
 		}
-		else 
-		{
-			_putchar(*format);
-			count++;
-		}	
+		else
+			count += print_char(*format);
+
 		format++;
 	}
+
 	va_end(args);
 	return (count);
 }
--- main.h
+++ main.h
@@ -1,10 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <stdarg.h>
+#include <stddef.h>
 
+int _putchar(char c);
 int _printf(const char *format, ...);
-
+int print_char(int c);
+int print_string(char *str);
 
 
 #endif
