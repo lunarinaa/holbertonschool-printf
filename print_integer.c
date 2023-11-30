6b36f156f46f65cc4e7afe94536eff14577f28fb
lunarinaa <aydan.akbarova@gmail.com>
2023-11-30 00:43:55 -0800

    edited according to betty doc style

--- print_integer.c
+++ print_integer.c
@@ -1,18 +1,22 @@
 #include "main.h"
 /**
+* print_integer - function to print integer numbers
+* @n: number to be printed
 *
-*
+* Return: number of printed elements
 */
 int print_integer(int n)
 {
-    int count = 0;
-    if (n < 0)
-    {
-        count += print_char('-');
-        n = -n;
-    }
-    if (n / 10)
-        count += print_integer(n / 10);
-    count += print_char(n % 10 + '0');
-    return (count);
+	int count = 0;
+
+	if (n < 0)
+	{
+		count += print_char('-');
+		n = -n;
+	}
+	if (n / 10)
+		count += print_integer(n / 10);
+	count += print_char(n % 10 + '0');
+	return (count);
 }
+
